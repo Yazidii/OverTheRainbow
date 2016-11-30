b72a35d2c31f4f81332e2fc4ae3193bada726560
Rinisra <ir76@ee.kent.ac.uk>
2016-11-30 17:42:16 +0000

    Crouch when spotted
    Trying to make the lights work again

--- Assets/Scripts/Spotted.cs
+++ Assets/Scripts/Spotted.cs
@@ -1,4 +1,35 @@
-﻿using UnityEngine;
+﻿/*using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+public class Spotted : MonoBehaviour
+{
+
+    ThirdPersonUserControl userControl;
+
+    // Use this for initialization
+    void Start()
+    {
+        userControl = GetComponentInParent<ThirdPersonUserControl>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Spotlight"))
+        {
+            //Destroy(other.gameObject);
+            userControl.ChangeCaughtState(true);
+        }
+    }
+}*/
+
+using UnityEngine;
 using System.Collections;
 using UnityStandardAssets.Characters.ThirdPerson;
 
@@ -28,4 +59,6 @@ public class Spotted : MonoBehaviour
         }
     }
 }
+
+
 
