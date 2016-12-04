0bf0d1dc1878bd9f736d20d50bceb61053cc2543
Rinisra <ir76@ee.kent.ac.uk>
2016-12-04 13:53:12 +0000

    Main Menu and GOMenu Updated
    Waiting for more assets

--- Assets/Scripts/CreditScript.cs
+++ Assets/Scripts/CreditScript.cs
@@ -5,13 +5,51 @@ public class CreditScript : MonoBehaviour {
 
 
     public Canvas CreditCanvas;
+    public GameObject Developers;
+    public GameObject specialThanks;
+    public GameObject Sources;
 
+    public void Start()
+    {
+        specialThanks.gameObject.SetActive(false);
+        Sources.gameObject.SetActive(false);
+        Developers.gameObject.SetActive(true);
+    }
     public void showCredit()
     {
         CreditCanvas.GetComponent<Canvas>().enabled = true;
+        Developers.gameObject.SetActive(true);
+        Sources.gameObject.SetActive(false);
+        specialThanks.gameObject.SetActive(false);
     }
+
+
     public void hideCredit()
     {
         CreditCanvas.GetComponent<Canvas>().enabled = false;
+        Developers.gameObject.SetActive(false);
+        Sources.gameObject.SetActive(false);
+        specialThanks.gameObject.SetActive(false);
+    }
+
+    public void showDevelopers()
+    {        
+        Sources.gameObject.SetActive(false);
+        specialThanks.gameObject.SetActive(false);
+        Developers.gameObject.SetActive(true);
+    }
+
+    public void showSpecialThanks()
+    {
+        Developers.gameObject.SetActive(false);
+        Sources.gameObject.SetActive(false);
+        specialThanks.gameObject.SetActive(true);
+    }
+    
+    public void showSources()
+    {
+        specialThanks.gameObject.SetActive(false);
+        Developers.gameObject.SetActive(false);
+        Sources.gameObject.SetActive(true);
     }
 }
