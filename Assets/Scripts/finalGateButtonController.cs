90cae2d63a10110eb47e5748db0ff8e24589d89c
Yazidi <mariusdmts@gmail.com>
2016-12-07 23:26:47 +0200

    Final Gate button + gate opening

---
+++ Assets/Scripts/finalGateButtonController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class finalGateButtonController : MonoBehaviour {
+
+    public List<GameObject> list;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Find("ButtonTop").gameObject.SetActive(ButtonState());
+        transform.Find("ButtonBottom").gameObject.SetActive(ButtonState());
+    }
+
+    bool ButtonState()
+    {
+        bool result = true;
+        foreach (GameObject piece in list)
+        {
+            if (!piece.GetComponent<MeshRenderer>().enabled) result = false;
+        }
+        return result;
+    }
+}
---
+++ Assets/Scripts/finalGateController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class finalGateController : MonoBehaviour {
+
+    public GameObject buttonController;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.GetComponent<Animator>().SetBool("finish", buttonController.GetComponent<buttonController>().isPressed);
+    }
+}
